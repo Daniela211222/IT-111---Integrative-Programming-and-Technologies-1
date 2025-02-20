bd23430a3f0bf64948978b2b0e93cc7518d3691c
Daniela Calpe <120469807+Daniela211222@users.noreply.github.com>
2025-02-20 09:59:44 +0800

    Add files via upload

---
+++ L4 Hands-on Loops & Conditionals/numberChecker.js
@@ -0,0 +1,10 @@
+function checkEvenOdd(num) {
+    if (num % 2 === 0) {
+        console.log(`${num} is even.`);
+    } else {
+        console.log(`${num} is odd.`);
+    }
+}
+
+checkEvenOdd(4);
+checkEvenOdd(7);
